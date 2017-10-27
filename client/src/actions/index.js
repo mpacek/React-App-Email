6f338546a58ee83fd3f5572e41c3f47414dbb651
mpacek <m.pacek@onet.eu>
2017-10-27 11:43:53 +0200

    Client action in progress

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-// The code below before refactoring:
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+// The code above before refactoring, the same functionality:
 //
 // export const fetchUser = () => {
 //   return function(dispatch) {
@@ -10,8 +15,3 @@ import { FETCH_USER } from './types';
 //       .then(res => dispatch({ type: FETCH_USER, payload: res }));
 //   };
 // };
-
-export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res.data });
-};
