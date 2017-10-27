1ed1ed224ee39238ada958c17b5fc1c1c3af1023
mpacek <m.pacek@onet.eu>
2017-10-27 11:56:01 +0200

    Client login in progress

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <div className="row">
@@ -8,11 +28,7 @@ class Header extends Component {
           <div className="nav-wrapper">
             <div className="col s12">
               <a className="left brand-logo">Emaily</a>
-              <ul className="right">
-                <li>
-                  <a>Login With Google</a>
-                </li>
-              </ul>
+              <ul className="right">{this.renderContent()}</ul>
             </div>
           </div>
         </nav>
@@ -21,4 +37,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
