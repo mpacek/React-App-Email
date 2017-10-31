65dfce685e23b84f04cd7188e4ec3f8985c8391e
mpacek <m.pacek@onet.eu>
2017-10-31 11:47:25 +0100

    Client payment action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res.data });
-};
-
-// The code above before refactoring, the same functionality:
+// The code below before refactoring, the same functionality:
 //
 // export const fetchUser = () => {
 //   return function(dispatch) {
@@ -15,3 +10,13 @@ export const fetchUser = () => async dispatch => {
 //       .then(res => dispatch({ type: FETCH_USER, payload: res }));
 //   };
 // };
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('apip/stripe', token);
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
