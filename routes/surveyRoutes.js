3d3157e72e0183c7724e8958e981a5c25633b376
mpacek <m.pacek@onet.eu>
2017-11-16 14:14:19 +0100

    Survey creation in progress

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,1 +1,20 @@
-module.exports = app => {};
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
+
+module.exports = app => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
+};
