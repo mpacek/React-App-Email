475b57a96e7dfe5bf18c1c2273a168b6667a7952
mpacek <m.pacek@onet.eu>
2017-11-24 20:21:55 +0100

    Mailer service in progress

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,3 +1,7 @@
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
-const keyes = require('../config/keys.js');
+const keyes = require('../config/keys');
+
+class Mailer extends helper.Mail {}
+
+module.exports = Mailer;
