b9fc02f5070d772fba9e71ebb1554ee7691237ee
mpacek <m.pacek@onet.eu>
2018-07-12 21:51:21 +0200

    SurveyForm in progress

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,38 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipients list', name: 'emails' }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
   render() {
     return (
       <div>
-        <Field type="text" name="surveyTitle" component="input" />
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button class="btn waves-effect waves-light" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
