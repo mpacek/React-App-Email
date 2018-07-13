458fdca893a90cf7bac8b8e656ada0b00619c041
mpacek <m.pacek@onet.eu>
2018-07-13 21:19:54 +0200

    Added submit survey action; clear the form; survey review

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, SUBMIT_SURVEY } from './types';
 
 // The code below before refactoring, the same functionality:
 //
@@ -19,4 +19,8 @@ export const fetchUser = () => async dispatch => {
 export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = values => {
+  return { type: SUBMIT_SURVEY };
 };
