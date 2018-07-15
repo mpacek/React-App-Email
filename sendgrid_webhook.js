580eb48fab170906346152cc609ac8c2cb948f29
mpacek <m.pacek@onet.eu>
2018-07-15 12:16:13 +0200

    Update of the db with new answers

--- sendgrid_webhook.js
+++ sendgrid_webhook.js
@@ -1,4 +1,4 @@
 var localtunnel = require('localtunnel');
-localtunnel(5000, { subdomain: 'packotunel2' }, function(err, tunnel) {
+localtunnel(5000, { subdomain: 'packotunel3' }, function(err, tunnel) {
   console.log('LT running');
 });
