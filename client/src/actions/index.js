16bec8a20c648b5018b889892faef9a49d1b2fa3
mpacek <m.pacek@onet.eu>
2018-07-15 21:25:28 +0200

    APP Final!

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // The code below before refactoring, the same functionality:
 //
@@ -28,4 +28,10 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
